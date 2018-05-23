178197d8a2c47057eb7030506371321b31280d09
Vitalii Onufryk <vitalii.onufryk@synapse.com>
2018-05-23 18:23:38 +0300

    Added TODO comments. New last file lines.

--- bot.rb
+++ bot.rb
@@ -1,11 +1,19 @@
 require './bot_dependencies'
 
-config = JSON.parse(File.read('config.json')) # reading config from config.json file
-puts config.inspect
+# TODO: move this to some Utils method (e.g. FileUtils::read_config)
+config = JSON.parse(File.read('config.json')) # read config.json
 
+# TODO: move to methods of some Bot custom operations
 page_html = Nokogiri::HTML(open(config['basecamp_url'])) # => Nokogiri::HTML::Document
 all_news = page_html.css('.news-card').map { |news_card| NewsCard.new(news_card) }
 all_news.each {|card| puts "Title: #{card.title}\nLink: #{card.link}\n"}
+
+# TODO in Bot menu:
+# Show all news
+# Show only BaseCamp news
+  # Show 1, 2, 3 etc. page
+# TODO for deploying (later)
+# use Heroku
 
 exit
 Telegram::Bot::Client.run(config['bot_token']) do |bot|
